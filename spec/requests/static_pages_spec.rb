4f8b923bb63c25dc7e88a30fcec39079c5123436
sasi <sasirekha@freshdesk.com>
2017-03-14 15:33:46 +0530

    add user following

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,14 +11,31 @@ describe "Static pages" do
     it { should have_selector('title', text: full_title(''))}
     it { should_not have_selector 'title', text: '| Home' }
 
-    describe "for singed_in users" do
+    describe "Help page" do
+      before { visit help_path }
+      it { should have_selector('h1', text: 'Help')}
+      it { should have_selector('title', text: full_title('Help'))}
+    end
+
+    describe "About page" do
+      before { visit about_path }
+      it { should have_selector('h1', text: 'About')}
+      it { should have_selector('title', text: full_title('About'))}
+    end
+
+    describe "Contact page" do
+      before { visit contact_path }
+      it { should have_selector('h1', text: 'Contact')}
+      it { should have_selector('title', text: full_title('Contact'))}
+    end
+
+    describe "for singed-in users" do
       let(:user) { FactoryGirl.create(:user)}
       before do
         FactoryGirl.create(:micrpost, user: user, content: "Lorem ipsum")
         FactoryGirl.create(:micropost, user: user, content: "Daffafaesf")
         sign_in user
         visit root_path
-
       end
 
       it "should render the user's feed" do
@@ -26,34 +43,16 @@ describe "Static pages" do
            page.should have_selector("li##{item.id}", text: item.content)
          end
       end
-    end
 
-  end
-
-  describe "Help page" do
-
-    before { visit help_path }
-
-    it { should have_selector('h1', text: 'Help')}
-    it { should have_selector('title', text: full_title('Help'))}
-
-  end
-
-
-  describe "About page" do
-    before { visit about_path }
-
-    it { should have_selector('h1', text: 'About')}
-    it { should have_selector('title', text: full_title('About'))}
-
-  end
-
-  describe "Contact page" do
-    before { visit contact_path }
-
-    it { should have_selector('h1', text: 'Contact')}
-    it { should have_selector('title', text: full_title('Contact'))}
-
-  end
-
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+      it { should have_link("0 following", href: following_user_path(user)) }
+      it { should have_link("1 follower", href: followers_user_path(user)) }
+      end
+   end
+ end
 end
